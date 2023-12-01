ee63c7b23239e7e660260f0385a124a935dd8ea7
FabioSimoDev <fabiofabietto182@gmail.com>
2023-12-01 14:46:54 +0100

    risolto bug, sistemato indice

--- src/player/MediaPlayer.java
+++ src/player/MediaPlayer.java
@@ -10,6 +10,9 @@ public class MediaPlayer {
     }
 
     public void executeElement(int index){
+        if(!(index - 1 < 0)) index--; //cosi posso usare l'index partendo da 1 e non da 0.
+        else throw new IllegalArgumentException("l'indice non può essere 0.");
+
         if(index >= 0 && index < elements.length){
             if(elements[index] instanceof Reproducible){
                 ((Reproducible) elements[index]).play();
